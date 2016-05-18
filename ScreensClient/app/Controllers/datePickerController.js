24d2e55937a3c13dddcc7731aa8dd620c4c66d1a
Alexander Kvasz <akvasz@swisslex.ch>
2016-05-18 16:25:04 +0200

    bug fix for date saving

--- ScreensClient/app/Controllers/datePickerController.js
+++ ScreensClient/app/Controllers/datePickerController.js
@@ -3,7 +3,7 @@
 app.controller("datePickerController", function ($scope) {
     $scope.dateOptions = {
         dateDisabled: false,
-        formatYear: 'yy',
+//        formatYear: 'yy',
         maxDate: new Date(2020, 5, 22),
         minDate: new Date(),
         startingDay: 1
--- ScreensClient/app/Controllers/schedulesController.js
+++ ScreensClient/app/Controllers/schedulesController.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-app.controller("schedulesController", function ($scope, dataService) {
+app.controller("schedulesController", function ($scope, dataService, $filter) {
 
     var tablename = 'schedules';
     // inherits $scope.filesInDb
@@ -9,19 +9,31 @@ app.controller("schedulesController", function ($scope, dataService) {
         return dataService.crudGetRecords(tablename)
             .then(function (response) {
                 $scope.schedules = response.data;
+                $scope.schedules.forEach(function(schedule) {
+                    if (schedule.data.once.date) schedule.data.once.date = new Date(schedule.data.once.date);
+                    if (schedule.data.validity.dateStart) schedule.data.validity.dateStart = new Date(schedule.data.validity.dateStart);
+                    if (schedule.data.validity.dateEnd) schedule.data.validity.dateEnd = new Date(schedule.data.validity.dateEnd);
+                });
             });
     };
     initDBSchedules();
 
 
     var initEmptyScheduleElement = function () {
-        $scope.newSchedule = { id: "", data: { name: '', type: 'once', typeForDay: 'hourlyForDay', once: {}, daily: { frequency: 1 }, weekly: { frequency: 1 }, monthly: { frequency: 'premier' }, monthlyii: { frequency: 1 } } };
+        $scope.newSchedule = {
+            id: "", data: {
+                name: '', type: 'once', typeForDay: 'hourlyForDay',
+                once: {}, daily: { frequency: 1 }, weekly: { frequency: 1 }, monthly: { frequency: 'premier' }, monthlyii: { frequency: 1 },
+                onceForDay: {}, minutelyForDay: {}, hourlyForDay: {}, validityinday: {}, validity: { dateStart: new Date() }
+            }
+        };
         $scope.scheduleselected = $scope.newSchedule;
     }
     initEmptyScheduleElement();
 
     $scope.saveSchedule = function () {
         var schedule = $scope.scheduleselected;
+        //schedule.data.once.date = $filter('date')(schedule.data.once.date, 'MM/dd/yyyy');
         if (schedule && schedule.data && !String.IsNullOrEmpty(schedule.data.name)) {
             var func = schedule.id
                 ? function () { return dataService.crudUpdateRecord(tablename, schedule.id, schedule.data); }
