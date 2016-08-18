485d048c2915dba0cf1d91af4ee58f11ad6cefe9
Alexander Kvasz <akvasz@swisslex.ch>
2016-08-18 19:10:13 +0200

    WelcomeNewGuys: prototype for angular

---
+++ WelcomeNewGuys/public/app/Controllers/mainController.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+app.controller("mainController",
+    function ($scope, dataService, configService) {
+    $scope.welcomeText = 'hello stranger';
+    
+    dataService.crudGetRecords('Employees').then(
+        function(response) {
+            $scope.employees = response.data;
+        }
+    );
+});
---
+++ WelcomeNewGuys/public/app/Services/configService.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+app.factory('configService', ['$location', function ($location) {
+
+    var getServerPath = function () {
+        var urlprefix = 'http://localhost:1337/data/';
+        return urlprefix;
+    }
+
+    var configServiceFactory = {};
+
+    configServiceFactory.getServerPath = getServerPath;
+    return configServiceFactory;
+}]);
